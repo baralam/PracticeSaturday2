b61684dc53dbb269cdf1515c1ddeea7370d787a2
baralam <baralam@sheridancollege.ca>
2020-06-20 21:32:42 -0400

    third commit

--- src/practicesat2/PracticeSat2.java
+++ src/practicesat2/PracticeSat2.java
@@ -8,6 +8,7 @@ public class PracticeSat2 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello world first person");
+         System.out.println("Hello world second person");
     }
     
 }
